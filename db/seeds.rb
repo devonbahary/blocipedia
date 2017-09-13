0412ff3742780d8317774cbecccffe75ad53f305
devonbahary <devonbahary@gmail.com>
2017-09-13 00:13:29 +0000

    User Story 6: Seeding Data | Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,25 @@
 require 'random_data'
+require 'faker'
+
+# Create Users 
+10.times do 
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: Faker::Internet.password
+  )
+end
 
 # Create Wikis
 50.times do 
   Wiki.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph,
-    private: RandomData.random_boolean
+    title: Faker::HitchhikersGuideToTheGalaxy.starship,
+    body: Faker::HitchhikersGuideToTheGalaxy.quote,
+    private: Faker::Boolean.boolean
   )
 end 
 
 
 
 puts "Seeds finished"
+puts "#{User.count} users created!"
 puts "#{Wiki.count} wikis created!"
